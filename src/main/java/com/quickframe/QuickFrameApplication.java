fbd4ee2266384f2ead82b0fc85e2dd6d50ce8add
Bishwo Gurung <bishwo.gurung3@gmail.com>
2020-02-25 21:41:49 -0500

    Create entry point for the application to start

---
+++ src/main/java/com/quickframe/QuickFrameApplication.java
@@ -0,0 +1,12 @@
+package com.quickframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickFrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickFrameApplication.class, args);
+    }
+}
